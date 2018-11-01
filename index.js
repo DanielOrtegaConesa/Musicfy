802feee1e363404eace4d583604f30b2f8512001
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-11-01 16:50:46 +0100

    creacion de los ficheros app.js e index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+var mongoose = require("mongoose");
+var app = require("./app");
+var port = 3977;
+
+mongoose.connect("mongodb://localhost:27017/musicfy", (err, res) => {
+    if(err){
+        throw err;
+    }else{
+        console.log("La conexion a la base de datos esta funcionando correctamente...");
+        app.listen(port, function(){
+        console.log("Servidor del api rest esuchando peticiones http:/localhost:"+port);
+        });
+    }
+})
