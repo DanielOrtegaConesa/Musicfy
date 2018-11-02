d9c40b6af0b7ee78d1026ccd317136760968e31f
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-11-02 17:24:58 +0100

    Update services/jwt.js

--- services/jwt.js
+++ services/jwt.js
@@ -2,7 +2,7 @@
 
 var jwt = require("jwt-simple");
 var moment = require("moment");
-var clave = "la clave mas segura del mundo!"
+var clave = "la clave mas segura del mundo!";
 
 exports.createTokern = function(user){
     var payload = {
