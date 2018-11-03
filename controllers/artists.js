34614578a526ff5165f3d87118b39766d0d1cbfa
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-11-03 20:05:25 +0100

    paginacion de artistas

--- controllers/artists.js
+++ controllers/artists.js
@@ -6,6 +6,7 @@ var path = require("path");
 var Artist = require("../models/artist");
 var Album = require("../models/album");
 var Song = require("../models/song");
+var mongoosePaginate = require("mongoose-pagination");
 
 function getArtist(req, res){
     var artistId = req.params.id;
@@ -18,6 +19,31 @@ function getArtist(req, res){
                 res.status(400).send({message: "El artista no existe"});
             }else{
                 res.status(200).send({artist});
+            }
+        }
+    });
+}
+
+function getArtists(req, res){
+    if(req.params.page){
+        var page = req.params.page;
+    }else{
+        var page = 1;
+    }
+    
+    var itemsPerPage = 3;
+
+    Artist.find().sort("name").paginate(page, itemsPerPage, (err, artists, total) => {
+        if(err){
+            res.status(500).send({message: "Error en la peticion"});
+        }else{
+            if(!artists){
+                res.status(400).send({message: "No hay artistas"});
+            }else{
+                return res.status(200).send({
+                    total_items: total,
+                    artists: artists
+                });
             }
         }
     });
@@ -46,5 +72,6 @@ function saveArtist(req, res){
 
 module.exports = {
     getArtist,
-    saveArtist
+    saveArtist,
+    getArtists
 };
--- routes/artists.js
+++ routes/artists.js
@@ -8,5 +8,6 @@ var md_auth = require("../middlewares/autenticated");
 
 api.get("/artist/:id", md_auth.ensureAuth, ArtistController.getArtist);
 api.post("/artist", md_auth.ensureAuth, ArtistController.saveArtist);
+api.get("/artists/:page?", md_auth.ensureAuth, ArtistController.getArtists);
 
 module.exports = api;
