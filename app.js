cf5ed331ddeb6356875a6eb201f9154a2633b0c0
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-11-03 19:26:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 
 // cargar rutas
 var user_routes = require("./routes/user");
+var artists_routes = require("./routes/artists");
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -14,6 +15,6 @@ app.use(bodyParser.json());
 // configurar cabeceras http
 
 // rutas base
-app.use("/api", user_routes);
+app.use("/api", [user_routes, artists_routes]);
 
 module.exports = app;
