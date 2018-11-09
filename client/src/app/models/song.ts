dcc131d4180407b4d4f48f45a431b6a898118153
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-11-09 18:44:07 +0100

    cambio de directorio e instalacion de angular

---
+++ client/src/app/models/song.ts
@@ -0,0 +1,9 @@
+export class Song{
+    constructor(
+        public number: number,
+        public title: string,
+        public duration: string,
+        public file: string,
+        public album: string
+    ){}
+}
