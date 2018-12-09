20cb9bc8aed30450850f836716f0c857ed5740e3
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-12-10 00:53:12 +0100

    Update app.routing.ts

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,20 +3,15 @@ import {Routes, RouterModule} from "@angular/router";
 
 
 import {UserEditComponent} from "./user-edit/user-edit.component";
-
 import {ArtistListComponent} from "./artist-list/artist-list.component";
+import {HomeComponent} from "./home/home.component";
 
 
 const appRoutes: Routes = [
-    {
-        path: "",
-        redirectTo: "/artists/1",
-        pathMatch: "full"
-    },
-    {path: "", component: ArtistListComponent},
+    {path: "", component: HomeComponent},
     {path: "artists/:page", component: ArtistListComponent},
     {path: "mis-datos", component: UserEditComponent},
-    {path: "**", component: ArtistListComponent}
+    {path: "**", component: HomeComponent}
 ];
 
 export const AppRoutingProviders: any[] = [];
