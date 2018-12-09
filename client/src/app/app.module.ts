a41f1d135354b2304d3724110e57a40647320ab9
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-12-09 15:19:01 +0100

    ArtistListComponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HttpModule } from '@angular/http';
 import { UserEditComponent } from './user-edit/user-edit.component';
 
 import {routing, AppRoutingProviders} from "./app.routing";
+import { ArtistListComponent } from './artist-list/artist-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserEditComponent
+    UserEditComponent,
+    ArtistListComponent
   ],
   imports: [
     BrowserModule,
