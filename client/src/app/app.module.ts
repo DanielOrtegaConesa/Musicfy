06f35cab2c35bcfc65a07611faf149cdc0084657
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-12-10 20:01:15 +0100

    Update app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { UserEditComponent } from './user-edit/user-edit.component';
 import {routing, AppRoutingProviders} from "./app.routing";
 import { ArtistListComponent } from './artist-list/artist-list.component';
 import { HomeComponent } from './home/home.component';
+import { ArtistAddComponent } from './artist-add/artist-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserEditComponent,
     ArtistListComponent,
-    HomeComponent
+    HomeComponent,
+    ArtistAddComponent
   ],
   imports: [
     BrowserModule,
