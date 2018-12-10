560499a4bc16742bd4901affb0d971d761d10399
Daniel Ortega Conesa <ortegaconesadaniel@gmail.com>
2018-12-10 20:01:37 +0100

    indentaciones

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,17 +1,19 @@
-import {ModuleWithProviders} from "@angular/core";
-import {Routes, RouterModule} from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 
-import {UserEditComponent} from "./user-edit/user-edit.component";
-import {ArtistListComponent} from "./artist-list/artist-list.component";
-import {HomeComponent} from "./home/home.component";
+import { UserEditComponent } from "./user-edit/user-edit.component";
+import { ArtistListComponent } from "./artist-list/artist-list.component";
+import { HomeComponent } from "./home/home.component";
+import { ArtistAddComponent } from "./artist-add/artist-add.component";
 
 
 const appRoutes: Routes = [
-    {path: "", component: HomeComponent},
-    {path: "artists/:page", component: ArtistListComponent},
-    {path: "mis-datos", component: UserEditComponent},
-    {path: "**", component: HomeComponent}
+    { path: "", component: HomeComponent },
+    { path: "artistas/:page", component: ArtistListComponent },
+    { path: "crear-artista", component: ArtistAddComponent },
+    { path: "mis-datos", component: UserEditComponent },
+    { path: "**", component: HomeComponent }
 ];
 
 export const AppRoutingProviders: any[] = [];
---
+++ client/src/app/artist-add/artist-add.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Global } from "../services/global";
+import { UserService } from "../services/user.service";
+import { Artist } from "../models/artist";
+@Component({
+  selector: 'artist-add',
+  templateUrl: './artist-add.component.html',
+  styleUrls: ['./artist-add.component.css']
+})
+export class ArtistAddComponent implements OnInit {
+  public titulo: string;
+  public artist: Artist;
+  public identity;
+  public token;
+  public url;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService) {
+    this.titulo = "Crear nuevo artista";
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getIdentity();
+    this.url = Global.url;
+
+    this.artist = new Artist();
+  }
+
+  ngOnInit() {
+  }
+
+}
